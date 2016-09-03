ad07da2c36deadd44d16c4553c32b1ecef284c36
Matej Gomboc <matejg03@gmail.com>
2016-09-03 16:29:51 +0200

    Initial commit

---
+++ IRremote_port/IRremote/IRremote.c
@@ -0,0 +1,205 @@
+//******************************************************************************
+// IRremote
+// Version 2.0.1 June, 2015
+// Copyright 2009 Ken Shirriff
+// For details, see http://arcfn.com/2009/08/multi-protocol-infrared-remote-library.html
+//
+// Modified by Paul Stoffregen <paul@pjrc.com> to support other boards and timers
+// Modified  by Mitra Ardron <mitra@mitra.biz>
+// Added Sanyo and Mitsubishi controllers
+// Modified Sony to spot the repeat codes that some Sony's send
+//
+// Interrupt code based on NECIRrcv by Joe Knapp
+// http://www.arduino.cc/cgi-bin/yabb2/YaBB.pl?num=1210243556
+// Also influenced by http://zovirl.com/2008/11/12/building-a-universal-remote-with-an-arduino/
+//
+// JVC and Panasonic protocol added by Kristian Lauszus (Thanks to zenwheel and other people at the original blog post)
+// LG added by Darryl Smith (based on the JVC protocol)
+// Whynter A/C ARC-110WD added by Francesco Meschia
+//******************************************************************************
+
+// Defining IR_GLOBAL here allows us to declare the instantiation of global variables
+#define IR_GLOBAL
+#	include "IRremote.h"
+#	include "IRremoteInt.h"
+#undef IR_GLOBAL
+
+//+=============================================================================
+// The match functions were (apparently) originally MACROs to improve code speed
+//   (although this would have bloated the code) hence the names being CAPS
+// A later release implemented debug output and so they needed to be converted
+//   to functions.
+// I tried to implement a dual-compile mode (DEBUG/non-DEBUG) but for some
+//   reason, no matter what I did I could not get them to function as macros again.
+// I have found a *lot* of bugs in the Arduino compiler over the last few weeks,
+//   and I am currently assuming that one of these bugs is my problem.
+// I may revisit this code at a later date and look at the assembler produced
+//   in a hope of finding out what is going on, but for now they will remain as
+//   functions even in non-DEBUG mode
+//
+int IR_MATCH (int measured,  int desired)
+{
+	IR_DBG_PRINT(F("Testing: "));
+	IR_DBG_PRINT(IR_TICKS_LOW(desired), DEC);
+	IR_DBG_PRINT(F(" <= "));
+	IR_DBG_PRINT(measured, DEC);
+	IR_DBG_PRINT(F(" <= "));
+	IR_DBG_PRINT(IR_TICKS_HIGH(desired), DEC);
+
+	uint8_t passed = ((measured >= IR_TICKS_LOW(desired)) && (measured <= IR_TICKS_HIGH(desired)));
+
+	if (passed)
+		IR_DBG_PRINTLN(F("?; passed"));
+	else
+		IR_DBG_PRINTLN(F("?; FAILED"));
+
+	return passed;
+}
+
+//+========================================================
+// Due to sensor lag, when received, Marks tend to be 100us too long
+//
+int IR_MATCH_MARK (int measured_ticks,  int desired_us)
+{
+	IR_DBG_PRINT("Testing mark (actual vs desired): ");
+	IR_DBG_PRINT(measured_ticks * IR_USECPERTICK);
+	IR_DBG_PRINT("us vs ");
+	IR_DBG_PRINT(desired_us);
+	IR_DBG_PRINT("us");
+	IR_DBG_PRINT(": ");
+	IR_DBG_PRINT(IR_TICKS_LOW(desired_us + IR_MARK_EXCESS) * IR_USECPERTICK);
+	IR_DBG_PRINT(" <= ");
+	IR_DBG_PRINT(measured_ticks * IR_USECPERTICK);
+	IR_DBG_PRINT(" <= ");
+	IR_DBG_PRINT(IR_TICKS_HIGH(desired_us + IR_MARK_EXCESS) * IR_USECPERTICK);
+
+	uint8_t passed = ((measured_ticks >= IR_TICKS_LOW (desired_us + IR_MARK_EXCESS))
+			&& (measured_ticks <= IR_TICKS_HIGH(desired_us + IR_MARK_EXCESS)));
+
+	if (passed)
+		IR_DBG_PRINTLN("?; passed");
+	else
+		IR_DBG_PRINTLN("?; FAILED");
+
+	return passed;
+}
+
+//+========================================================
+// Due to sensor lag, when received, Spaces tend to be 100us too short
+//
+int IR_MATCH_SPACE (int measured_ticks,  int desired_us)
+{
+	IR_DBG_PRINT("Testing space (actual vs desired): ");
+	IR_DBG_PRINT(measured_ticks * IR_USECPERTICK);
+	IR_DBG_PRINT("us vs ");
+	IR_DBG_PRINT(desired_us);
+	IR_DBG_PRINT("us");
+	IR_DBG_PRINT(": ");
+	IR_DBG_PRINT(IR_TICKS_LOW(desired_us - IR_MARK_EXCESS) * IR_USECPERTICK);
+	IR_DBG_PRINT(" <= ");
+	IR_DBG_PRINT(measured_ticks * IR_USECPERTICK);
+	IR_DBG_PRINT(" <= ");
+	IR_DBG_PRINT(IR_TICKS_HIGH(desired_us - IR_MARK_EXCESS) * IR_USECPERTICK);
+
+	uint8_t passed = ((measured_ticks >= IR_TICKS_LOW (desired_us - IR_MARK_EXCESS))
+			&& (measured_ticks <= IR_TICKS_HIGH(desired_us - IR_MARK_EXCESS)));
+
+	if (passed)
+		IR_DBG_PRINTLN("?; passed");
+	else
+		IR_DBG_PRINTLN("?; FAILED");
+
+	return passed;
+}
+
+//+=============================================================================
+// Interrupt Service Routine - Fires every 50uS
+// TIMER2 interrupt code to collect raw data.
+// Widths of alternating SPACE, MARK are recorded in rawbuf.
+// Recorded in ticks of 50uS [microseconds, 0.000050 seconds]
+// 'rawlen' counts the number of entries recorded so far.
+// First entry is the SPACE between transmissions.
+// As soon as a the first [SPACE] entry gets long:
+//   Ready is set; State switches to IDLE; Timing of SPACE continues.
+// As soon as first MARK arrives:
+//   Gap width is recorded; Ready is cleared; New logging starts
+//
+
+void IR_ISR ()
+{
+	IR_TIMER_RESET;
+
+	// Read if IR Receiver -> SPACE [xmt LED off] or a MARK [xmt LED on]
+	uint8_t irdata = (uint8_t)HAL_GPIO_ReadPin(irparams.recvpinport, irparams.recvpin);
+
+	irparams.timer++; // One more 50uS tick
+	if (irparams.rawlen >= IR_RAWBUF) irparams.rcvstate = IR_STATE_OVERFLOW ; // Buffer overflow
+
+	switch(irparams.rcvstate)
+	{
+		//......................................................................
+		case IR_STATE_IDLE: // In the middle of a gap
+			if (irdata == IR_MARK)
+			{
+				if (irparams.timer < IR_GAP_TICKS)
+				{  // Not big enough to be a gap.
+					irparams.timer = 0;
+				}
+				else
+				{
+					// Gap just ended; Record duration; Start recording transmission
+					irparams.overflow                  = 0;
+					irparams.rawlen                    = 0;
+					irparams.rawbuf[irparams.rawlen++] = irparams.timer;
+					irparams.timer                     = 0;
+					irparams.rcvstate                  = IR_STATE_MARK;
+				}
+			}
+			break;
+		//......................................................................
+		case IR_STATE_MARK:  // Timing Mark
+			if (irdata == IR_SPACE)
+			{   // Mark ended; Record time
+				irparams.rawbuf[irparams.rawlen++] = irparams.timer;
+				irparams.timer                     = 0;
+				irparams.rcvstate                  = IR_STATE_SPACE;
+			}
+			break;
+		//......................................................................
+		case IR_STATE_SPACE:  // Timing Space
+			if (irdata == IR_MARK)
+			{   // Space just ended; Record time
+				irparams.rawbuf[irparams.rawlen++] = irparams.timer;
+				irparams.timer                     = 0;
+				irparams.rcvstate                  = IR_STATE_MARK;
+			}
+			else if (irparams.timer > IR_GAP_TICKS)
+			{   // Space
+				// A long Space, indicates gap between codes
+				// Flag the current code as ready for processing
+				// Switch to STOP
+				// Don't reset timer; keep counting Space width
+				irparams.rcvstate = IR_STATE_STOP;
+			}
+			break;
+		//......................................................................
+		case IR_STATE_STOP:  // Waiting; Measuring Gap
+		 	if (irdata == IR_MARK)  irparams.timer = 0 ;  // Reset gap timer
+		 	break;
+		//......................................................................
+		case IR_STATE_OVERFLOW:  // Flag up a read overflow; Stop the State Machine
+			irparams.overflow = 1;
+			irparams.rcvstate = IR_STATE_STOP;
+		 	break;
+	}
+
+	// If requested, flash LED while receiving IR data
+	if (irparams.blinkflag)
+	{
+		if (irdata == IR_MARK)
+		{
+			if (irparams.blinkpin) HAL_GPIO_WritePin(irparams.blinkpinport, irparams.blinkpin, GPIO_PIN_SET); // Turn user defined pin LED on
+		}
+		else if (irparams.blinkpin) HAL_GPIO_WritePin(irparams.blinkpinport, irparams.blinkpin, GPIO_PIN_RESET); // Turn user defined pin LED on
+	}
+}
---
+++ IRremote_port/IRremote/IRremoteInt.h
@@ -0,0 +1,161 @@
+//******************************************************************************
+// IRremote
+// Version 2.0.1 June, 2015
+// Copyright 2009 Ken Shirriff
+// For details, see http://arcfn.com/2009/08/multi-protocol-infrared-remote-library.html
+//
+// Modified by Paul Stoffregen <paul@pjrc.com> to support other boards and timers
+//
+// Interrupt code based on NECIRrcv by Joe Knapp
+// http://www.arduino.cc/cgi-bin/yabb2/YaBB.pl?num=1210243556
+// Also influenced by http://zovirl.com/2008/11/12/building-a-universal-remote-with-an-arduino/
+//
+// JVC and Panasonic protocol added by Kristian Lauszus (Thanks to zenwheel and other people at the original blog post)
+// Whynter A/C ARC-110WD added by Francesco Meschia
+//******************************************************************************
+
+#ifndef IRremoteint_h
+#define IRremoteint_h
+
+#include <stdint.h>
+#include "gpio.h"
+
+//------------------------------------------------------------------------------
+// This handles definition and access to global variables
+//
+#ifdef IR_GLOBAL
+#	define IR_EXTERN
+#else
+#	define IR_EXTERN extern
+#endif
+
+//------------------------------------------------------------------------------
+// Information for the Interrupt Service Routine
+//
+#define IR_RAWBUF  101  // Maximum length of raw duration buffer
+
+typedef struct
+{
+	// The fields are ordered to reduce memory over caused by struct-padding
+	uint8_t       rcvstate;     // State Machine state
+	uint16_t      recvpin;      // Pin connected to IR data from detector
+	GPIO_TypeDef* recvpinport;  // Port of pin connected to IR data from detector
+	uint16_t      blinkpin;
+	GPIO_TypeDef* blinkpinport;
+	uint8_t       blinkflag;          // true -> enable blinking of pin on IR processing
+	uint8_t       rawlen;             // counter of entries in rawbuf
+	unsigned int  timer;              // State timer, counts 50uS ticks.
+	unsigned int  rawbuf[IR_RAWBUF];  // raw data
+	uint8_t       overflow;           // Raw buffer overflow occurred
+} irparams_t;
+
+// ISR State-Machine : Receiver States
+#define IR_STATE_IDLE      2
+#define IR_STATE_MARK      3
+#define IR_STATE_SPACE     4
+#define IR_STATE_STOP      5
+#define IR_STATE_OVERFLOW  6
+
+// Allow all parts of the code access to the ISR data
+// NB. The data can be changed by the ISR at any time, even mid-function
+// Therefore we declare it as "volatile" to stop the compiler/CPU caching it
+IR_EXTERN volatile irparams_t irparams;
+
+//------------------------------------------------------------------------------
+// CPU Frequency
+//
+//#ifdef F_CPU
+//#	define SYSCLOCK  F_CPU     // main Arduino clock
+//#else
+//#	define SYSCLOCK  16000000  // main Arduino clock
+//#endif
+
+////------------------------------------------------------------------------------
+//// Defines for setting and clearing register bits
+////
+//#ifndef cbi
+//#	define cbi(sfr, bit)  (_SFR_BYTE(sfr) &= ~_BV(bit))
+//#endif
+//
+//#ifndef sbi
+//#	define sbi(sfr, bit)  (_SFR_BYTE(sfr) |= _BV(bit))
+//#endif
+
+//------------------------------------------------------------------------------
+// Pulse parms are ((X*50)-100) for the Mark and ((X*50)+100) for the Space.
+// First MARK is the one after the long gap
+// Pulse parameters in uSec
+//
+
+// Due to sensor lag, when received, Marks  tend to be 100us too long and
+//                                   Spaces tend to be 100us too short
+#define IR_MARK_EXCESS    100
+
+// microseconds per clock interrupt tick
+#define IR_USECPERTICK    50
+
+// Upper and Lower percentage tolerances in measurements
+#define IR_TOLERANCE       25
+#define IR_LTOL            (1.0 - (IR_TOLERANCE/100.))
+#define IR_UTOL            (1.0 + (IR_TOLERANCE/100.))
+
+// Minimum gap between IR transmissions
+#define IR_GAP            5000
+#define IR_GAP_TICKS       (IR_GAP/IR_USECPERTICK)
+
+#define IR_TICKS_LOW(us)   ((int)(((us)*IR_LTOL/IR_USECPERTICK)))
+#define IR_TICKS_HIGH(us)  ((int)(((us)*IR_UTOL/IR_USECPERTICK + 1)))
+
+//------------------------------------------------------------------------------
+// IR detector output is active low
+//
+#define IR_MARK   0
+#define IR_SPACE  1
+
+//------------------------------------------------------------------------------
+// Defines for Timer
+
+//---------------------------------------------------------
+// Timer2 (8 bits)
+//
+#if defined(IR_USE_TIMER2)
+
+	#define IR_TIMER_RESET
+	#define IR_TIMER_ENABLE_PWM    (TCCR2A |= _BV(COM2B1))
+	#define IR_TIMER_DISABLE_PWM   (TCCR2A &= ~(_BV(COM2B1)))
+	#define IR_TIMER_ENABLE_INTR   (TIMSK2 = _BV(OCIE2A))
+	#define IR_TIMER_DISABLE_INTR  (TIMSK2 = 0)
+	#define IR_TIMER_INTR_NAME     TIMER2_COMPA_vect
+
+	#define IR_TIMER_CONFIG_KHZ(val) ({ \
+		const uint8_t pwmval = SYSCLOCK / 2000 / (val); \
+		TCCR2A               = _BV(WGM20); \
+		TCCR2B               = _BV(WGM22) | _BV(CS20); \
+		OCR2A                = pwmval; \
+		OCR2B                = pwmval / 3; \
+	})
+
+	#define IR_TIMER_COUNT_TOP  (SYSCLOCK * USECPERTICK / 1000000)
+
+	//-----------------
+	#if (IR_TIMER_COUNT_TOP < 256)
+		#	define IR_TIMER_CONFIG_NORMAL() ({ \
+				TCCR2A = _BV(WGM21); \
+				TCCR2B = _BV(CS20); \
+				OCR2A  = TIMER_COUNT_TOP; \
+				TCNT2  = 0; \
+			})
+	#else
+		#	define IR_TIMER_CONFIG_NORMAL() ({ \
+				TCCR2A = _BV(WGM21); \
+				TCCR2B = _BV(CS21); \
+				OCR2A  = IR_TIMER_COUNT_TOP / 8; \
+				TCNT2  = 0; \
+			})
+	#endif
+
+	//-----------------
+	#define IR_TIMER_PWM_PIN  3              // Arduino Duemilanove, Diecimila, LilyPad, etc
+
+#endif
+#endif
